457048dac1b86355865f74d7a61e2937b88c145e
Okello Leon <okellopatrickleon@gmail.com>
2023-06-13 15:26:41 +0200

    Made the Help more responsive put the help button in a better position

--- KnC.js
+++ KnC.js
@@ -186,7 +186,22 @@ boxesCol.forEach(box=>{
     })
     
 })
-        
+
+function myFunction() {
+    // Get the text field
+    var copyText = document.getElementById("myInput");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+  }
+
 helpBtn.addEventListener("click",(e)=>{
     help.style.display = "flex"
 })
